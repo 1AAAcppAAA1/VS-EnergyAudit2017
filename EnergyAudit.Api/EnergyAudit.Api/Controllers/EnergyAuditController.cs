0de706b49aac775cd7247fdad0d97cfd2ac99c24
Taylor Smith <tcs140230@utdallas.edu>
2017-05-13 18:12:10 -0500

    copy of api from original project

---
+++ EnergyAudit.Api/EnergyAudit.Api/Controllers/EnergyAuditController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EnergyAudit.Api.Data;
+using EnergyAudit.Api.Model;
+
+namespace EnergyAudit.Api.Controllers
+{
+    [Route("api/energyaudit")]
+    public class EnergyAuditController : Controller
+    {
+        // GET api/values
+        // include calculations for difference in amperage
+        // return value of difference
+        [HttpGet]
+        [Route("test")]
+        public IEnumerable<Appliance> Get()
+        {
+            var client = new DataClient();
+            var appliances = client.Test();
+            return appliances;
+        }
+
+        // http post request
+        // move current appliance to previous appliance
+        // send current appliance
+        // store inside of current appliance...
+
+    }
+}
